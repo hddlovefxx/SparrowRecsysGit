76f2b2e77b76a9b9ed6e7623b3c9d6bec94a7320
hddlovefxx <hdd1073394498@gmail.com>
2021-01-12 15:37:44 +0800

    start

--- SparrowRecSys-master/testJava.java
+++
@@ -1,7 +0,0 @@
-import jdk.internal.vm.PostVMInitHook;
-
-public class testJava {
-    public static void main(String[] args) {
-        System.out.print("I am Hello Class, which is a main class");
-    }
-}
